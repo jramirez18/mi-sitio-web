21ceb53a21cfe9a7a9fe8d6624d6e6d14d07fc95
Jorge Ramirez <jorramirez_18@hotmail.com>
2015-10-10 15:00:25 -0600

    Agregue los archivos de BASE, DETALLE PUBLICACIONES, Modificacion en el archivo detalle_publicacion, url y view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, get_object_or_404 #ERROR QUE DAN LAS PAGINAS WEB CUANDO INTENTAMOS ENTRAR A UNA PAG Q NO EXISTE
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Postear
@@ -6,3 +7,7 @@ from .models import Postear
 def listar_publicaciones(request):
     publicaciones= Postear.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/listar_publicaciones.html', {'publicaciones':publicaciones})
+
+def detalle_publicaciones(request, pk):
+    publicacion = get_object_or_404(Postear, pk=pk)
+    return render(request, 'blog/detalle_publicaciones.html', {'publicacion': publicacion})
