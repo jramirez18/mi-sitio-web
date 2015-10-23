13931e04145c0565b9ce706a58ffaa4cee04c15f
Jorge Ramirez <jorramirez_18@hotmail.com>
2015-10-22 21:09:38 -0600

    Creacion de nueva url para el detalle de un post, creacion de detalle publicaciones, creacion de la nueva vista detalle

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
 from . import views
 
+
 urlpatterns = [
     url(r'^$', views.listar_publicaciones),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.detalle_publicaciones),
+    url (r'^post/(?P<pk>[0-9]+)/$', views.detalle_publicaciones),
+    url(r'^post/new/$', views.nuevo_post, name='nuevo_post'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.editar_post, name='editar_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404 #ERROR QUE DAN LAS PAGINAS WEB CUANDO INTENTAMOS ENTRAR A UNA PAG Q NO EXISTE
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Postear
+from .forms import PostForm
 
 # Create your views here.
 def listar_publicaciones(request):
@@ -9,5 +11,30 @@ def listar_publicaciones(request):
     return render(request, 'blog/listar_publicaciones.html', {'publicaciones':publicaciones})
 
 def detalle_publicaciones(request, pk):
-    publicacion = get_object_or_404(Postear, pk=pk)
-    return render(request, 'blog/detalle_publicaciones.html', {'publicacion': publicacion})
+    post = get_object_or_404(Postear, pk=pk)
+    return render(request, 'blog/detalle_publicaciones.html', {'post': post})
+
+def nuevo_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.save()
+            return redirect('blog.views.detalle_publicaciones', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/editar_post.html', {'form': form})
+
+def editar_post(request, pk):
+        post = get_object_or_404(Postear, pk=pk)
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.autor = request.user
+                post.save()
+                return redirect('blog.views.detalle_publicaciones', pk=post.pk)
+        else:
+            form = PostForm(instance=post)
+        return render(request, 'blog/editar_post.html', {'form': form})
